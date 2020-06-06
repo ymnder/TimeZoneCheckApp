e0c1c33eae1181f478c3879f3515bd2b1e7e81a2
ymnder <yamazaki.waseda@gmail.com>
2020-06-07 01:17:02 +0900

    add JodaTime

--- app/src/main/java/com/ymnd/timezonecheckapp/MyApplication.kt
+++ app/src/main/java/com/ymnd/timezonecheckapp/MyApplication.kt
@@ -2,10 +2,12 @@ package com.ymnd.timezonecheckapp
 
 import android.app.Application
 import com.jakewharton.threetenabp.AndroidThreeTen
+import net.danlew.android.joda.JodaTimeAndroid
 
 class MyApplication: Application() {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
+        JodaTimeAndroid.init(this)
     }
 }
