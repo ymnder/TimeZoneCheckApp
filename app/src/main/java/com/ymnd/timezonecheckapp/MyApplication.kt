1eb38dc9c3199ed8bd618a1a810bc9b6285fb37a
ymnder <yamazaki.waseda@gmail.com>
2020-06-07 10:57:21 +0900

    fix multidex support

--- app/src/main/java/com/ymnd/timezonecheckapp/MyApplication.kt
+++ app/src/main/java/com/ymnd/timezonecheckapp/MyApplication.kt
@@ -1,10 +1,10 @@
 package com.ymnd.timezonecheckapp
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.jakewharton.threetenabp.AndroidThreeTen
 import net.danlew.android.joda.JodaTimeAndroid
 
-class MyApplication: Application() {
+class MyApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
