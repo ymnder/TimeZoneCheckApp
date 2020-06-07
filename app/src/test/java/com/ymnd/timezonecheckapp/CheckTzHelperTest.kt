06beddaec8febbb303fb1d46e09f7177dc5a4602
ymnder <yamazaki.waseda@gmail.com>
2020-06-07 11:26:40 +0900

    add robolectric

---
+++ app/src/test/java/com/ymnd/timezonecheckapp/CheckTzHelperTest.kt
@@ -0,0 +1,20 @@
+package com.ymnd.timezonecheckapp
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CheckTzHelperTest {
+
+    @Test
+    fun printDeviceOsVersion() {
+    }
+
+    @Test
+    fun printTzVersion() {
+    }
+
+    @Test
+    fun printDateTime() {
+    }
+}
